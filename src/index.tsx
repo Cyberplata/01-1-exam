3f5d28e517989b440f6856488677df8b080f7370
PlatonKynin <kininplaton@gmail.com>
2024-02-05 22:28:39 +0300

    1. task - section section nav?

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,73 @@
 import ReactDOM from 'react-dom/client';
-import styled from 'styled-components';
+import styled from "styled-components";
 
+const StyledApp = styled.div`
+    color: white;
+    display: grid;
+    height: 100vh;
+    grid-template-columns: 250px 1fr;
+    grid-template-rows: 60px 1fr 60px;
+    grid-template-areas: 
+    "h h"
+    "s c"
+    "f f"
+`
+const StyledHeader = styled.header`
+    background-color: #5555ef;
+    padding: 20px;
+    display: flex;
+    align-items: center;
+    grid-area: h;
 
-const Camera = XXX.YYY`
-    font-size: 1.5em;
-    text-align: center;
-    color: #e91e63;
-`;
-
-function Box() {
-    return (
-        <Camera>Affair</Camera>
-    );
-}
-
+    a {
+        color: white
+    }
+`
+const StyledContent = styled.section`
+    color: #1f1d1d;
+    padding: 020px;
+    grid-area: c
+    
+`
+const StyledSidebar = styled.section`
+    border-right: 1px solid silver;
+    color: #1f1d1d;
+    grid-area: s;
+    
+`
+const StyledFooter = styled.footer`
+    background-color: #5555ef;
+    padding: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    grid-area: f;
+`
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-    <div className="App">
-        <Box />
-    </div>
+    <StyledApp>
+        <StyledHeader>
+            <a href="#">appointment-logo</a>
+        </StyledHeader>
+        <StyledSidebar>
+            <nav>
+                <ul>
+                    <li><a href="">amount</a></li>
+                    <li><a href="">analysis</a></li>
+                    <li><a href="">courage</a></li>
+                    <li><a href="">car</a></li>
+                </ul>
+            </nav>
+        </StyledSidebar>
+        <StyledContent>
+            <h1>
+                airport
+            </h1>
+        </StyledContent>
+        <StyledFooter>©access</StyledFooter>
+    </StyledApp>
 );
 
-
-
-//Что должно быть вместо XXX и YYY, чтобы отобразился заголовок первого уровня?
-//❗ В ответе укажите значения через пробел, вот так: XXX YYY
+// Что должно быть вместо XXX, YYY и ZZZ, чтобы разметка была семантическая?
+// ❗ В ответе укажите значения через пробел, вот так: XXX YYY ZZZ
