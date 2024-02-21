84f0fd8f73e361babdd0e258c8acdda006b3f292
PlatonKynin <kininplaton@gmail.com>
2024-02-21 20:07:20 +0300

    01-1-4-exam-html/css-for-react

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,35 @@
 import ReactDOM from 'react-dom/client';
-import styled from 'styled-components';
+import styled, { css } from "styled-components";
 
 
-const Advice = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    color: #e91e63;
-`;
+const StyledContext = styled.a`
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 25px;
+    white-space: nowrap;
+    color: #2182d9;
+`
 
-function Ability() {
-    return (
-        <Advice>Business</Advice>
-    );
+type ContextPropsType = {
+    path: string
+    text: string
 }
 
+function Context(props:ContextPropsType) {
+    return (
+        <StyledContext href={props.path}>
+            {props.text}
+        </StyledContext>
+    );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <div className="App">
-        <Ability />
+        <Context path="https://hotmart.s3.amazonaws.com/product_pictures/ccdb08be-d4d5-467b-8491-a88bf61024bc/guiahtml38051.png" text="See Context" />
     </div>
 );
 
 
-
-//Что должно быть вместо XXX и YYY, чтобы отобразился заголовок первого уровня?
-//❗ В ответе укажите значения через пробел, вот так: XXX YYY
+// Что должно быть вместо XXX, YYY и ZZZ, чтобы код работал корректно?
+// ❗ В ответе укажите значения через пробел: XXX YYY ZZZ
