6e04edf065189f848a21a30c80c4fd3e23ea3551
PlatonKynin <kininplaton@gmail.com>
2024-02-21 20:11:23 +0300

    01-1-5-exam-html/css-for-react

--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,73 @@
 import ReactDOM from 'react-dom/client';
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
-
-const StyledContext = styled.a`
-    font-weight: 700;
-    font-size: 18px;
-    line-height: 25px;
-    white-space: nowrap;
-    color: #2182d9;
+const StyledApp = styled.div`
+    color: white;
+    display: grid;
+    height: 100vh;
+    grid-template-columns: 250px 1fr;
+    grid-template-rows: 60px 1fr 60px;
+    grid-template-areas: 
+    "h h"
+    "s c"
+    "f f"
 `
+const StyledHeader = styled.header`
+    background-color: #5555ef;
+    padding: 20px;
+    display: flex;
+    align-items: center;
+    grid-area: h;
 
-type ContextPropsType = {
-    path: string
-    text: string
-}
-
-function Context(props:ContextPropsType) {
-    return (
-        <StyledContext href={props.path}>
-            {props.text}
-        </StyledContext>
-    );
-}
+    a {
+        color: white
+    }
+`
+const StyledContent = styled.section`
+    color: #1f1d1d;
+    padding: 020px;
+    grid-area: c
+    
+`
+const StyledSidebar = styled.aside`
+    border-right: 1px solid silver;
+    color: #1f1d1d;
+    grid-area: s;
+    
+`
+const StyledFooter = styled.footer`
+    background-color: #5555ef;
+    padding: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    grid-area: f;
+`
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-    <div className="App">
-        <Context path="https://hotmart.s3.amazonaws.com/product_pictures/ccdb08be-d4d5-467b-8491-a88bf61024bc/guiahtml38051.png" text="See Context" />
-    </div>
+    <StyledApp>
+        <StyledHeader>
+            <a href="#">answer-logo</a>
+        </StyledHeader>
+        <StyledSidebar>
+            <nav>
+                <ul>
+                    <li><a href="">cookie</a></li>
+                    <li><a href="">analyst</a></li>
+                    <li><a href="">chocolate</a></li>
+                    <li><a href="">company</a></li>
+                </ul>
+            </nav>
+        </StyledSidebar>
+        <StyledContent>
+            <h1>
+                collection
+            </h1>
+        </StyledContent>
+        <StyledFooter>©breath</StyledFooter>
+    </StyledApp>
 );
 
-
-// Что должно быть вместо XXX, YYY и ZZZ, чтобы код работал корректно?
-// ❗ В ответе укажите значения через пробел: XXX YYY ZZZ
+// Что должно быть вместо XXX, YYY и ZZZ, чтобы разметка была семантическая?
+// ❗ В ответе укажите значения через пробел, вот так: XXX YYY ZZZ
