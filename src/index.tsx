cdb3923d99f5021f7a75ada977526fc4736fec8c
PlatonKynin <kininplaton@gmail.com>
2024-02-21 20:28:04 +0300

    01-2-4-exam-html/css-for-react

--- src/index.tsx
+++ src/index.tsx
@@ -1,43 +1,63 @@
-import styled from "styled-components";
 import ReactDOM from 'react-dom/client';
+import styled, { css } from "styled-components";
 
 
-const StyledCousinInput = styled.input`
-    width: 350px;
-    height: 35px;
-    background-color: transparent;
-    border: 1px solid #626673;
-    border-radius: 5px;
-    caret-color: #2d2f38;
-    padding: 8px 20px;
+const StyledBonus = styled.button<AccountPropsType>`
+    padding: 8px 16px;
+    border-radius: 4px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    text-align: center;
     font-weight: 700;
-    font-size: 16px;
-    line-height: 20px;
-    color: #2d2f38;
+    font-size: 18px;
+    line-height: 25px;
+    cursor: pointer;
+    white-space: nowrap;
+    box-sizing: border-box;
 
-    &::placeholder {
-        font-weight: 600;
-        font-size: 16px;
-        line-height: 20px;
-        color: #a3a9ba;
-        background-color: red;
-    }
-`
+    ${(props) =>
+            props.accountType === "primary" &&
+            css`
+      color: #ffffff;
+      background-color: #2182d9;
+    `}
 
+    ${(props) =>
+            props.accountType === "secondary" &&
+            css`
+      background-color: transparent;
+      color: #2182d9;
+      border: 2px solid #2182d9;
+      &:hover {
+        background-color: #2182d9;
+        color: #ffffff;
+      }
+    `}
+`;
 
-const CousinInput = () => {
-    return (
-        <StyledCousinInput placeholder="Type cousin here..."/>
-    );
+type AccountPropsType = {
+    accountType?: "primary" | "secondary"
+    disabled?: boolean
 };
 
+function Account(props: AccountPropsType) {
+    const { accountType, disabled} = props;
+    return (
+        <StyledBonus accountType={accountType} disabled={disabled}>
+            Cat
+        </StyledBonus>
+    );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <div className="App">
-        <CousinInput />
+        <Account accountType="secondary" />
     </div>
 );
 
 
-// Что должно быть вместо XXX, чтобы стили применились к подсказке в текстовом поле?
+// Что должно быть вместо XXX и YYY, чтобы код работал корректно?
+// ❗ В ответе укажите значения через пробел, вот так: XXX YYY
