850649f95d2d9beda779abebbdf208026e3bcdd9
PlatonKynin <kininplaton@gmail.com>
2024-02-21 20:43:25 +0300

    01-3-3-exam-html/css-for-react

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,26 @@
 import ReactDOM from 'react-dom/client';
 import styled from "styled-components";
 
+export const StyledBusiness = styled.div`
+    background: url("https://cdn.iconscout.com/icon/free/png-256/free-css3-8-1175200.png") center/cover no-repeat;
 
-const BirthImg = styled.img`
-    height: 400px;
-    width: 400px;
-    object-fit: contain
-`
-
+    @media (orientation: landscape), (min-width: 1200px) {
+        width: 500px;
+        height: 500px;
+    }
+    @media  (orientation: portrait), (max-width: 768px) {
+        width: 250px;
+        height: 250px;
+    }
+`;
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <div className="App">
-        <BirthImg
-            src="https://cdn.iconscout.com/icon/free/png-256/free-node-js-1-1174935.png?f=webp"
-
-            srcSet="https://brandlogos.net/wp-content/uploads/2020/09/react-logo-512x512.png 2x, https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png 3x" />
+        <StyledBusiness />
     </div>
 );
 
 
-// Что должно быть вместо XXX, YYY и ZZZ, чтобы код работал корректно и для экранов с разной плотностью пикселей загружалось правильное изображение?
-//❗ В ответе укажите значения через пробел, вот так: XXX YYY ZZZ
+// Какие значения должны быть вместо XXX, YYY и ZZZ, чтобы для элемента большое изображение было на десктопе или ландшафтной ориентации, а маленькое - на мобилках или портретой ориентации?
+// ❗ В ответе укажите значения через пробел вот так: XXX YYY ZZZ
